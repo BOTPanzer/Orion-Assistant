ec8ab1097b8154dbe21100ca916f16b83b93b91a
Alex <panitroll33@gmail.com>
2025-02-11 13:43:47 +0100

    started with gallery module

---
+++ Modules/Gallery/start.js
@@ -0,0 +1,68 @@
+ /*$    /$$                             
+| $$   | $$                             
+| $$   | $$ /$$$$$$   /$$$$$$   /$$$$$$$
+|  $$ / $$/|____  $$ /$$__  $$ /$$_____/
+ \  $$ $$/  /$$$$$$$| $$  \__/|  $$$$$$ 
+  \  $$$/  /$$__  $$| $$       \____  $$
+   \  $/  |  $$$$$$$| $$       /$$$$$$$/
+    \_/    \_______/|__/      |______*/ 
+
+var galleryModule = cModule
+var galleryWSS = null
+var galleryWS = null
+
+
+ /*$$$$$$$                              /$$     /$$                              
+| $$_____/                             | $$    |__/                              
+| $$    /$$   /$$ /$$$$$$$   /$$$$$$$ /$$$$$$   /$$  /$$$$$$  /$$$$$$$   /$$$$$$$
+| $$$$$| $$  | $$| $$__  $$ /$$_____/|_  $$_/  | $$ /$$__  $$| $$__  $$ /$$_____/
+| $$__/| $$  | $$| $$  \ $$| $$        | $$    | $$| $$  \ $$| $$  \ $$|  $$$$$$ 
+| $$   | $$  | $$| $$  | $$| $$        | $$ /$$| $$| $$  | $$| $$  | $$ \____  $$
+| $$   |  $$$$$$/| $$  | $$|  $$$$$$$  |  $$$$/| $$|  $$$$$$/| $$  | $$ /$$$$$$$/
+|__/    \______/ |__/  |__/ \_______/   \___/  |__/ \______/ |__/  |__/|______*/ 
+
+function galleryStart() {
+  if (galleryWSS != null) {
+    console.log('Server is already running')
+    return
+  }
+
+  const { WebSocketServer } = require('ws')
+
+  console.log('Creating server')
+  galleryWSS = new WebSocketServer({ port: 8080 })
+
+  galleryWSS.on('connection', (ws) => {
+    console.log('connection open')
+    galleryWS = ws
+
+    ws.on('error', (error) => {
+      console.error(error)
+    })
+
+    ws.on('message', (data) => {
+      console.log(data)
+      console.log('received: %s', data)
+    })
+
+    ws.on('close', () => {
+      console.log('connection closed')
+    })
+
+    //ws.send('something')
+  })
+}
+
+
+  /*$$$$$                  /$$          
+ /$$__  $$                | $$          
+| $$  \__/  /$$$$$$   /$$$$$$$  /$$$$$$ 
+| $$       /$$__  $$ /$$__  $$ /$$__  $$
+| $$      | $$  \ $$| $$  | $$| $$$$$$$$
+| $$    $$| $$  | $$| $$  | $$| $$_____/
+|  $$$$$$/|  $$$$$$/|  $$$$$$$|  $$$$$$$
+  \______/  \______/  \_______/ \______*/
+
+
+//Module isn't hidden -> Try to start server
+if (!galleryModule.isHidden) galleryStart()
