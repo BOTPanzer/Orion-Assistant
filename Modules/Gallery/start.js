311446dd140c2c19600b8033616f845c1033030a
Alex <panitroll33@gmail.com>
2025-02-12 12:52:32 +0100

    android app shares album files names

--- Modules/Gallery/start.js
+++ Modules/Gallery/start.js
@@ -10,6 +10,13 @@
 var galleryModule = cModule
 var galleryWSS = null
 var galleryWS = null
+var gallery = {
+  app: {
+    connected: false,
+  },
+  albumsInfo: {},
+  albums: []
+}
 
 
  /*$$$$$$$                              /$$     /$$                              
@@ -22,38 +29,94 @@ var galleryWS = null
 |__/    \______/ |__/  |__/ \_______/   \___/  |__/ \______/ |__/  |__/|______*/ 
 
 function galleryStart() {
+  //Gallert server already running
   if (galleryWSS != null) {
     console.log('Server is already running')
     return
   }
 
-  const { WebSocketServer } = require('ws')
-
+  //Create server
   console.log('Creating server')
+  const { WebSocketServer } = require('ws')
   galleryWSS = new WebSocketServer({ port: 8080 })
 
+  //Add connection event
   galleryWSS.on('connection', (ws) => {
+    //Too many connections
+    if (galleryWSS.clients > 1) {
+      ws.close()
+      return
+    }
+
+    //Connected succesfully
     console.log('connection open')
-    console.log(ws)
+    gallery.app.connected = true
     galleryWS = ws
-
-    ws.on('error', (error) => {
-      console.error(error)
+    
+    //Socket message
+    ws.on('message', (data, isBinary) => {
+      if (isBinary)
+        galleryParseBinary(data)
+      else
+        galleryParseString(data)
     })
 
-    ws.on('message', (data) => {
-      console.log('received data')
-      //console.log(data)
-      fs.writeFileSync(galleryModule.path + 'image.jpg', data)
-      //console.log('received: %s', data)
-    })
-
+    //Socket closed or had an error
     ws.on('close', () => {
+      gallery.app.connected = false
       console.log('connection closed')
     })
 
-    //ws.send('something')
+    ws.on('error', (error) => {
+      gallery.app.connected = false
+      console.error(error)
+      ws.close()
+    })
   })
+}
+
+function galleryParseBinary(data) {
+  console.log(data)
+  //fs.writeFileSync(galleryModule.path + 'image.jpg', data)
+  //ws.send('something')
+}
+
+function galleryParseString(data) {
+  //Parse object
+  const object = JSON.parse(data.toString())
+  //console.log(object)
+
+  //Check action
+  switch (object.action) {
+    //Info about albums (how many are there, for example)
+    case 'albumsInfo': {
+      //Save albums info
+      gallery.albumsInfo = {
+        size: object.size,
+        received: 0,
+      }
+
+      //Resize albums array
+      gallery.albums.length = gallery.albumsInfo.size
+
+      //Request album files
+      galleryWS.send(JSON.stringify({
+        action: 'requestAlbums'
+      }))
+      break
+    }
+
+    //Received an album
+    case 'album': {
+      //Save album files
+      gallery.albums[object.index] = object.files
+
+      //Finished receiving albums?
+      gallery.albumsInfo.received++
+      if (gallery.albumsInfo.received >= gallery.albumsInfo.size) console.log("Received all albums")
+      break
+    }
+  }
 }
 
 
