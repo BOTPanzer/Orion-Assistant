98fc25e6a03880979f8ddb894e0929164e339461
Alex <panitroll33@gmail.com>
2025-05-09 21:04:10 +0200

    updated stickers module to use a sqlite database to save local copies of the stickers

--- Modules/Stickers/start.js
+++ Modules/Stickers/start.js
@@ -10,36 +10,36 @@
 //Create stickers shortcut
 const stickersModuleMainPath = cModule.mainPath
 createShortcut(() => {
-  //Screen info
-  const mouse = screen.getCursorScreenPoint()
-  const display = screen.getDisplayNearestPoint(mouse)
+    //Screen info
+    const mouse = screen.getCursorScreenPoint()
+    const display = screen.getDisplayNearestPoint(mouse)
 
-  //Window info
-  const margin = 80
-  const size = {
-    width: 720,
-    height: 460
-  }
+    //Window info
+    const margin = 80
+    const size = {
+        width: 720,
+        height: 460
+    }
 
-  //Get position with margin around screen borders
-  const pos = {
-    x: clamp(mouse.x - size.width / 2, 
-             display.bounds.x + margin,
-             display.bounds.x - margin - size.width + display.bounds.width),
-    y: clamp(mouse.y - size.height / 2, 
-             display.bounds.y + margin, 
-             display.bounds.y - margin - size.height + display.bounds.height),
-  }
+    //Get position with margin around screen borders
+    const pos = {
+        x: clamp(mouse.x - size.width / 2, 
+                 display.bounds.x + margin,
+                 display.bounds.x - margin - size.width + display.bounds.width),
+        y: clamp(mouse.y - size.height / 2, 
+                 display.bounds.y + margin, 
+                 display.bounds.y - margin - size.height + display.bounds.height),
+    }
 
-  //Create window
-  createOrionWindow(stickersModuleMainPath, null, {
-    width: size.width, 
-    height: size.height, 
-    x: pos.x,
-    y: pos.y,
-    fullscreenable: false,
-  })
+    //Create window
+    createOrionWindow(stickersModuleMainPath, null, {
+        width: size.width, 
+        height: size.height, 
+        x: pos.x,
+        y: pos.y,
+        fullscreenable: false,
+    })
 }, 'CommandOrControl+Alt+S', {
-  tag: 'openStickers',
-  permanent: true
+    tag: 'openStickers',
+    permanent: true
 })
